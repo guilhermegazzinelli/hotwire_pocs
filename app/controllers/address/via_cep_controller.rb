ba6b117e1308a2115057ea7c3c6376626b359824
Guilherme Gazzinelli Rohrmann <guilherme.gazzinelli@gmail.com>
2024-04-29 14:59:36 -0300

    Adiciona Processamento por turbo stream

--- app/controllers/address/via_cep_controller.rb
+++ app/controllers/address/via_cep_controller.rb
@@ -7,15 +7,17 @@ class Address::ViaCepController < ApplicationController
 
     json = response.body
     respond_to do |format|
-      format.json {  render json: json, status: :ok }
-      format.turbo_stream {
+      format.json { render json:, status: :ok }
+      format.turbo_stream do
         ua = JSON.parse(json)
-        aux = { zip_code: ua["cep"], state: ua['uf'], city: ua['localidade'], other: nil}
+        aux = { zip_code: ua['cep'], state: ua['uf'], city: ua['localidade'],
+                other: [ua['logradouro'], ua['complemento'], ua['bairro']].compact_blank.join(', ') }
         ua = User.new
         ua.build_user_address(aux)
         render turbo_stream: [
-        turbo_stream.update("address", partial: "users/replace_form", locals: {user_address: ua.user_address})]
-      }
+          turbo_stream.update('address', partial: 'users/address/fields', locals: { user_address: ua.user_address })
+        ]
+      end
     end
   end
 end
